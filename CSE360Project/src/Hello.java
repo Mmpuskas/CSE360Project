3e33fa9eb6d2aa773ee94270d1ed2b4467ee0838
YifanLi5 <yifanli5@asu.edu>
2016-03-29 15:31:54 -0700

    print yifan li

--- CSE360Project/src/Hello.java
+++ CSE360Project/src/Hello.java
@@ -11,6 +11,7 @@ public class Hello
 		// TODO Auto-generated method stub
 		System.out.println("hello why is it not working");
 		System.out.println("Hi, this is studly Tony :)");
+		System.out.println("yifan Li");
 		
 
 	}
