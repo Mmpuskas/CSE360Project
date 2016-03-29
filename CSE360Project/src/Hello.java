28b69cf226cee1601d1c2454ca574766252c1b0a
Mercedes Perguson <mercedeslakail@gmail.com>
2016-03-29 15:21:53 -0700

    Updated format

--- CSE360Project/src/Hello.java
+++ CSE360Project/src/Hello.java
@@ -1,7 +1,13 @@
+/**
+ * @description
+ * @author Mercedes Perguson, Micheal, William, Tony, Yifan
+ *
+ */
+public class Hello 
+{
 
-public class Hello {
-
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 		System.out.println("hello why is it not working");
 		
