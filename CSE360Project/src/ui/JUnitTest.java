b5d9919dcbceb339d148b09a48711faef01b47d8
Will <wchen122@asu.edu>
2016-04-15 18:57:41 -0700

    WilliamChenJUnitTest

--- CSE360Project/src/ui/JUnitTest.java
+++ CSE360Project/src/ui/JUnitTest.java
@@ -7,35 +7,34 @@ import java.util.LinkedList;
 import org.junit.Test;
 
 public class JUnitTest {
-
+	UIControl control = new UIControl();
+	
 	@Test
-	public void testQuardinates(LinkedList<Tile> tileList) {
-		tileList.add(new Tile(262, 280));
-		tileList.add(new Tile(262, 380));
-		tileList.add(new Tile(262, 480));
-		tileList.add(new Tile(362, 490));
-		tileList.add(new Tile(462, 500));
-		tileList.add(new Tile(562, 500));
-		tileList.add(new Tile(562, 430));
-		tileList.add(new Tile(662, 430));
-		tileList.add(new Tile(742, 430));
-		tileList.add(new Tile(752, 330));
-		tileList.add(new Tile(762, 240));
-		tileList.add(new Tile(662, 240));
-		tileList.add(new Tile(662, 140));
-		tileList.add(new Tile(682, 60));
-		tileList.add(new Tile(782, 60));
-		tileList.add(new Tile(882, 60));
-		tileList.add(new Tile(972, 70));
-		tileList.add(new Tile(972, 170));
-		tileList.add(new Tile(962, 260));
-		tileList.add(new Tile(962, 350));
-		tileList.add(new Tile(1037, 350));
-		tileList.add(new Tile(1037, 430));
-		tileList.add(new Tile(1037, 520));
-		tileList.add(new Tile(1127, 530));
-		tileList.add(new Tile(1217, 540));
-		fail("Not yet implemented");
+	/*
+	 * Test whether or not the tileList is empty or not
+	 */
+	public void testEmpty()
+	{
+		control.initVals();
+		assertNotNull("The tile list is not empty", control);
 	}
+	
+	@Test
+	/*
+	 * Test whether the coordinates of the tile list is right
+	 */
+	public void testCoordinates() {
+		control.initVals();
+		//test the first tile
+		assertTrue("The first tile's x coordinate is 262", control.tileList.getFirst().x == 262);
+		assertTrue("The first tile's y coordinate is 280", control.tileList.getFirst().y == 280);
+		//test the middle tile
+		assertTrue("Index 13 of the tile's has an x coordinate of 682", control.tileList.get(13).x == 682);
+		assertTrue("Index 13 of the tile's has an x coordinate of 60", control.tileList.get(13).y == 60);
+		//test the last tile
+		assertTrue("The last tile's x coordinate is 1217", control.tileList.getLast().x == 1217);
+		assertTrue("The last tile's y coordinate is 540", control.tileList.getLast().y == 540);
+	}
+	
 
 }
