43047499ad76b17f4e1ac7c777c05d5a88205fc9
Mercedes Perguson <mercedeslakail@gmail.com>
2016-04-22 15:01:32 -0700

    adding leaderboard

---
+++ CSE360Project/src/ui/leaderboard.java
@@ -0,0 +1,56 @@
+package ui;
+/*
+ * Author: Mercedes
+ * Purpose: Keep track of the score of the user. 
+ * 	Also, utilize ScoreHistory.txt to save all scores and retrieve the highest top scores.
+ * 
+ */
+
+public class leaderboard
+{
+	private int totalScore;
+	private int highestScore;
+	
+	//default construct
+	public leaderboard()
+	{
+		totalScore = 0;
+		highestScore = 0;
+		
+	}
+
+	//getters & setters
+	public int getTotalScore() 
+	{
+		return totalScore;
+	}
+
+	public void setTotalScore(int totalScore) 
+	{
+		this.totalScore = totalScore;
+	}
+
+	public int getHighestScore() 
+	{
+		return highestScore;
+	}
+
+	public void setHighestScore(int highestScore) 
+	{
+		this.highestScore = highestScore;
+	}
+	
+	//functions that actually do stuff
+	
+	private void checkHighest(int tempScore)
+	{
+		
+	}
+	
+	private void saveScore()
+	{
+		
+	}
+	
+	
+}
