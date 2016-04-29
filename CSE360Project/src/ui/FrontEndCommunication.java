f70eaa30510e6244478c3850ae171eb7e246bfbd
Mmpuskas <mpuskas@asu.edu>
2016-04-29 14:27:04 -0700

    Added scores to UIControl, cleaned up events

--- CSE360Project/src/ui/FrontEndCommunication.java
+++ CSE360Project/src/ui/FrontEndCommunication.java
@@ -1,13 +1,9 @@
 package ui;
 
 public interface FrontEndCommunication { //for events and fights scenes?
-	//RNG
-	void roll();
-	public boolean getRollResults();
-	boolean reRoll();
 
 	//pass descriptions of events to frontend
 	public String getFlavorText();
 	public String getAftermath();
-	
+	public void setFlavorText(String flavorText);
 }
--- CSE360Project/src/ui/UIControl.java
+++ CSE360Project/src/ui/UIControl.java
@@ -16,6 +16,13 @@ import java.util.ListIterator;
  */
 public class UIControl 
 {
+	private int currentScore = 0;
+
+	public void getEvent()
+	{
+		
+	}
+	
 	LinkedList<Tile> tileList = new LinkedList<>();
 	ListIterator<Tile> tileIterator;
 	public void initTilePositions(int gameWidth, int gameHeight)
@@ -46,5 +53,23 @@ public class UIControl
 		tileList.add(new Tile((int) (0.733723958 * gameWidth), (int) (0.527363184 * gameHeight)));
 		tileList.add(new Tile((int) (0.792317708 * gameWidth), (int) (0.537313433 * gameHeight)));
 		tileIterator = tileList.listIterator();
+	}
+	
+	/** Fills tiles with event classes
+	 * 
+	 */
+	public void initEvents()
+	{
+		
+	}
+	
+	public void setScore(int score)
+	{
+		currentScore = score;
+	}
+	
+	public int getScore()
+	{
+		return currentScore;
 	}
 }
