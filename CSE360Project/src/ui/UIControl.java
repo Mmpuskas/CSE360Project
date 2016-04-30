6684be8edf6afc5cd06fdf9955acdaaf69f5225e
YifanLi5 <yifanli5@asu.edu>
2016-04-29 20:01:38 -0700

    set up file reading

--- CSE360Project/src/ui/UIControl.java
+++ CSE360Project/src/ui/UIControl.java
@@ -14,25 +14,24 @@ import java.util.ListIterator;
  * Creates a linkedlist of position and text data.
  * Class UIVisual creates an instance of this class to receive the data.
  */
-/** Control class for "Journey to Chaos End". Comprises most of the back-end systems.
+/**
+ * Control class for "Journey to Chaos End". Comprises most of the back-end
+ * systems.
  * 
  * @author Michael Puskas (mpuskas@asu.edu), Yifan Li
  *
  */
-public class UIControl 
-{
+public class UIControl {
 	private int currentScore = 0;
 
-	public void getEvent()
-	{
-		
-	}
-	
 	LinkedList<Tile> tileList = new LinkedList<>();
 	ListIterator<Tile> tileIterator;
-	public void initTilePositions(int gameWidth, int gameHeight)
-	{
-		tileList.add(new Tile((int) (0.170572917 * gameWidth), (int) (0.178606965 * gameHeight))); //TODO: Fix this position
+
+	public void initTilePositions(int gameWidth, int gameHeight) {
+		tileList.add(new Tile((int) (0.170572917 * gameWidth), (int) (0.178606965 * gameHeight))); // TODO:
+																									// Fix
+																									// this
+																									// position
 		tileList.add(new Tile((int) (0.170572917 * gameWidth), (int) (0.278606965 * gameHeight)));
 		tileList.add(new Tile((int) (0.170572917 * gameWidth), (int) (0.378109453 * gameHeight)));
 		tileList.add(new Tile((int) (0.170572917 * gameWidth), (int) (0.47761194 * gameHeight)));
@@ -60,60 +59,60 @@ public class UIControl
 		tileList.add(new Tile((int) (0.792317708 * gameWidth), (int) (0.537313433 * gameHeight)));
 		tileIterator = tileList.listIterator();
 	}
-	
-	/** Fills tiles with event classes
-	 * @throws IOException 
+
+	/**
+	 * Fills tiles with event classes
+	 * 
+	 * @throws IOException
 	 * 
 	 */
-	public void initEvents()
-	{
+	public void initEvents() {
 		String line = null;
 		String path = new File("").getAbsolutePath() + "/src/assets/EventText.txt";
 		String fullText = new String();
-	
-		try
-		{
+
+		try {
 			BufferedReader br = new BufferedReader(new FileReader(path));
-			while((line = br.readLine()) != null)
-			{
+			while ((line = br.readLine()) != null) {
 				fullText += line + "\n";
 			}
-		}
-		catch(IOException ex){
+		} catch (IOException ex) {
 			System.out.println("ERROR: Event file not found.");
 		}
-		
-		//Set intro text into first tile position
-		String introText = fullText.substring(fullText.indexOf('$') + 2, fullText.indexOf('@'));
-		
 
-		//Remove intro text from fullText
-		fullText = fullText.substring(fullText.indexOf(')') + 2);
-		
-		for(int i = 1; i <= 25; i++)
-		{
-			if( (i + 1) % 5 == 0)
-			{
-				
-				
-			}
-			else
-			{
-				
-				
+		// Set intro text into first tile position
+		String introText = fullText.substring(fullText.indexOf('$') + 2, fullText.indexOf('@'));
+		String intermediateText;
+
+		// Remove intro text from fullText
+
+		for (int i = 1; i <= 25; i++) {
+			fullText = fullText.substring(fullText.indexOf(')') + 2);
+			intermediateText = fullText.substring(0, fullText.indexOf(')') - 2);
+
+			if (i % 5 == 0) {
+				String[] eventDelimited = intermediateText.split("@");
+				String flavorText = eventDelimited[0].trim();
+				String passText = eventDelimited[1].trim();
+				String failText = eventDelimited[3].trim();
+				int passPoints = Integer.parseInt(eventDelimited[2].trim());
+				int failPoints = Integer.parseInt(eventDelimited[4].trim());
+				tileList.get(i).setBossEvent(flavorText, passText, failText, passPoints, failPoints);
+			} else {
+				String[] eventDelimited = intermediateText.split("@");
+				String flavorText = eventDelimited[0].trim();
+				int pointsChange = Integer.parseInt(eventDelimited[1].trim());
+				tileList.get(i).setGenericEvent(flavorText, pointsChange);
 			}
 		}
-		
-		//System.out.println(fullText);
+
 	}
-	
-	public void setScore(int score)
-	{
+
+	public void setScore(int score) {
 		currentScore = score;
 	}
-	
-	public int getScore()
-	{
+
+	public int getScore() {
 		return currentScore;
 	}
 }
