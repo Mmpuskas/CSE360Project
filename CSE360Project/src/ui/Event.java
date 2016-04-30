9ffa0b09c73f9f992857853203c6346090055774
YifanLi5 <yifanli5@asu.edu>
2016-04-29 17:14:17 -0700

    Updated Event and pressed the right buttton ths itme.

--- CSE360Project/src/ui/Event.java
+++ CSE360Project/src/ui/Event.java
@@ -1,91 +1,38 @@
 package ui;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
-public class Event implements FrontEndCommunication{
+public class Event{
 	//variables for handling events
 	//change final variables as desired
-	private String eventFlavorText;
-	private String passEventText;
-	private String failEventText;
-	private int pointsForSpecEvent;
-	private boolean passedEvent = false;
+	private String flavorText;
+	private int pointsChange;
 	
-	@Override
-	public String getAftermath() {
-		if(passedEvent){
-			return passEventText;
-		}
-		else{
-			return failEventText;
-		}
-		
+	public Event(String flavorText, int pointsChange){
+		this.flavorText = flavorText;
+		this.pointsChange = pointsChange;
 	}
 	
-	@Override
-	public void setFlavorText(String flavorText){
-		eventFlavorText = flavorText;
+	//getter/setter methods
+	public String getFlavorText() {
+		return flavorText;
 	}
 
-	@Override
-	public String getFlavorText(){
-		return eventFlavorText;
+
+	public void setFlavorText(String flavorText) {
+		this.flavorText = flavorText;
 	}
+
+
+	public int getPointsChange() {
+		return pointsChange;
+	}
+
+
+	public void setPointsChange(int pointsChange) {
+		this.pointsChange = pointsChange;
+	}
+
 	
-	@Override
-	public boolean equals(Object otherEvent){
-		if(otherEvent == this){
-			return true;
-		}
-		if(!(otherEvent instanceof Event)){ 
-			return false;
-		}
-		
-		Event test = (Event)otherEvent;
-		
-		if(eventFlavorText.equals(test.getEventFlavorText())
-			&& passEventText.equals(test.getPassEventText())
-			&& failEventText.equals(test.getFailEventText())
-			){
-			
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
 	
-	public void forceEqualsFail(){
-		eventFlavorText = "asdafasdf";
-	}
 	
-	//getter methods
-	public int getPointsSpecEvent(){
-		return pointsForSpecEvent;
-	}
 	
-	public String getEventFlavorText() {
-		return eventFlavorText;
-	}
-
-	public void setEventFlavorText(String eventFlavorText) {
-		this.eventFlavorText = eventFlavorText;
-	}
-
-	public String getPassEventText() {
-		return passEventText;
-	}
-
-	public void setPassEventText(String passEventText) {
-		this.passEventText = passEventText;
-	}
-
-	public String getFailEventText() {
-		return failEventText;
-	}
-
-	public void setFailEventText(String failEventText) {
-		this.failEventText = failEventText;
-	}
 }
--- CSE360Project/src/ui/FrontEndCommunication.java
+++
@@ -1,9 +0,0 @@
-package ui;
-
-public interface FrontEndCommunication { //for events and fights scenes?
-
-	//Pass descriptions of events to frontend
-	public String getFlavorText();
-	public String getAftermath();
-	public void setFlavorText(String flavorText);
-}
