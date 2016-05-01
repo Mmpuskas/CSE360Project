4c8f79affd1a9a813705074efa959f30b4d2b4c2
Will <wchen122@asu.edu>
2016-04-30 23:10:06 -0700

    sc

--- CSE360Project/src/ui/UIVisual.java
+++ CSE360Project/src/ui/UIVisual.java
@@ -272,7 +272,7 @@ public class UIVisual
 							txt.setPadding(new Insets(0,0,5,0));
 							vb.getChildren().add(txt);
 						}
-						//scrollbar
+						//scrollbars
 						sc = new ScrollBar();
 						sc.setMin(0);
 						sc.setMax(primaryScreenBounds.getHeight());
